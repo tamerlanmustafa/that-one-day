590a00dd5b21f77a8af2af72c5784c498bba386d
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2024-11-20 22:02:28 -0500

    remove extra details section, render more items from wiki api

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,33 +1,50 @@
 import { useState, useEffect } from "react"
-import { useState, useEffect } from "react"
 import './Hero.scss'
 
-const Hero = ({ onDateChange, data }) => {
+
 const Hero = ({ onDateChange, data }) => {
     const [year, setYear] = useState('')
-    const [temp, setTemp] = useState(null);
-    const [description, setDescription] = useState('');
-    const [event, setEvent] = useState(null);
 
- 
+    // const [event, setEvent] = useState(null);
+    // const [selected, setSelected] = useState(null);
+    // const [birth, setBirth] = useState(null);
+    // const [holiday, setHoliday] = useState(null);
+
+    const [apiData, setApiData] = useState({
+        event: '',
+        selected: '',
+        birth: '',
+        holiday: '',
+    })
+
+
     const handleChange = (e) => {
         const selectedDate = e.target.value;
         setYear(selectedDate); 
         onDateChange(selectedDate); 
     }
-
+    
 
     useEffect(() => {
-        if (data && data.length > 1) {
-
-            const { temp, description } = data[0].days[0]; 
-            const event = data[1]?.events[0].text;  
-            setTemp(temp);
-            setDescription(description);
-            setEvent(event);
+        if (data && data.length > 0) {
+            setApiData((prevState) => ({
+                ...prevState,
+                event: data[0]?.events?.[0]?.text || 'No event found',
+                selected: data[0]?.selected?.[0]?.text || 'No selected item found',
+                birth: data[0]?.births?.[0]?.text || 'No births found',
+                holiday: data[0]?.holidays?.[0]?.text || 'No holidays found',
+            }));
+        } else {
+            
+            setApiData({
+                event: 'No event found',
+                selected: 'No selected item found',
+                birth: 'No births found',
+                holiday: 'No holidays found',
+            });
         }
     }, [data]);
-
+ 
     return (
         <>
             <main>
@@ -49,25 +66,31 @@ const Hero = ({ onDateChange, data }) => {
 
                 <div className="that-day">
                     <div>
-                        <h3>On this day:</h3>
-                        <p>{event ? event : 'error'}</p>
+                        <h3>On this day:</h3> 
+                        <ul>
+                            <li>
+                                <h4>Event: </h4>
+                                {apiData.event ? apiData.event : 'error'}
+                            </li>
+                            <hr />
+                            <li>
+                                <h4>Selected: </h4>
+                                {apiData.selected ? apiData.selected : 'error'}
+                            </li>
+                            <hr />
+                            <li>
+                                <h4>Who was born: </h4>
+                                {apiData.birth ? apiData.birth : 'error'}
+                            </li>
+                            <hr />
+                            <li>
+                                <h4>Holiday:</h4>
+                                {apiData.holiday ? apiData.holiday : 'error'}
+                            </li>
+                        </ul>
+                        
                     </div>
-                    
-                    <div className="extra-details">
-                        <div>
-                            <h4>Weather:</h4>
-                            <h5>{temp ? `${temp} °F`: 'error 2' }</h5>
-                            <p> It was {description ? description : 'error 3' }</p>
-                        </div>
-                        <div>
-                            <h4>Top 3 songs: </h4>
-                            <ul>
-                                <li>Music</li>
-                                <li>Music</li>
-                                <li>Music</li>
-                            </ul>
-                        </div>
-                    </div>
+
                 </div>
                 
             </main>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,23 +7,16 @@ const Home =    () => {
     const [data, setData] = useState(null)
     const [date, setDate] = useState("");
     
-    // wikipedia
-    const fetchData = async () => {
-       
-    }
-    fetchData()
 
-    //////////////
+
     useEffect(() => {
         if (date) {
         
-            const API_KEY1 = import.meta.env.VITE_API_KEY_WEATHER
             const API_Authorization = import.meta.env.VITE_API_WIKI_AUTHORIZATION
             const [year, month, day] = date.split('-')
 
 
 
-            const apiUrl1 = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Arlington,VA/${year}-${month}-${day}T13:00:00?key=${API_KEY1}&include=current `
 
             const apiUrl2 = `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${month}/${day}`
 
@@ -36,22 +29,18 @@ const Home =    () => {
         
             const handleRequests = async () => {
                 try {
-                    const responseWeatherApi = await fetch(apiUrl1)
-                    const resWeatherData = await responseWeatherApi.json()
 
                     const responseWikiApi = await fetch(apiUrl2, optionsApi2)
                     const resWikiData = await responseWikiApi.json()
                     
                     const result = []
-                    result.push( resWeatherData, resWikiData)
+                    result.push(  resWikiData)
  
                     setData(result)
                    
-
                 } catch (err) {
                     console.log(err)
                 } finally {
-                    // console.log('Finally happened')
                 }
             }
             handleRequests()
