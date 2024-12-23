8184d7d8828463dd122e1e6a9b9d6720cad6afdc
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2024-12-22 19:24:06 -0500

    add loading functionality till the data is fully retreived

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,34 @@ import { useState, useEffect } from "react";
 import './Hero.scss';
 
 const Hero = ({ onDateChange, data }) => {
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        if (data && data.length > 0) {
+            setLoading(true); 
+    
+            setTimeout(() => {
+                setApiData({
+                    event: data[0]?.events?.[9]?.text || 'No event found',
+                    selected: data[0]?.selected?.[5]?.text || 'No selected item found',
+                    birth: data[0]?.births?.[0]?.text || 'No births found',
+                    death: data[0]?.deaths?.[0]?.text || 'No deaths found',
+                    holiday: data[0]?.holidays?.[0]?.text || 'No holidays found',
+                });
+    
+                setLoading(false); 
+            }, 1000);
+        } else {
+            setApiData({
+                event: 'No event found',
+                selected: 'No selected item found',
+                birth: 'No births found',
+                death: 'No deaths found',
+                holiday: 'No holidays found',
+            });
+            setLoading(false);
+        }
+    }, [data]);
+    
     const [year, setYear] = useState(''); 
     const [apiData, setApiData] = useState({
         event: '',
@@ -80,35 +108,40 @@ const Hero = ({ onDateChange, data }) => {
                 </div>
 
                 <div className="that-day">
-                    <div className="that-day__content">
-                        <h3>Key events on <strong>{formattedDate}</strong>:</h3>
-                        <ul>
-                            <li>
-                                <h4>Event: </h4>
-                                {apiData.event || 'error'}
-                            </li>
-                            <hr />
-                            <li>
-                                <h4>Selected: </h4>
-                                {apiData.selected || 'error'}
-                            </li>
-                            <hr />
-                            <li>
-                                <h4>Who was born: </h4>
-                                {apiData.birth || 'error'}
-                            </li>
-                            <hr />
-                            <li>
-                                <h4>Who died: </h4>
-                                {apiData.death || 'error'}
-                            </li>
-                            <hr />
-                            <li>
-                                <h4>Holiday:</h4>
-                                {apiData.holiday || 'error'}
-                            </li>
-                        </ul>
-                    </div>
+                    {loading ? (
+                        <div className="loading">
+                            <p>Loading data...</p>
+                        </div>
+                    ) : (
+                        <div className="that-day__content">
+                            <h3>Key events on <strong>{formattedDate}</strong>:</h3>
+                            <ul>
+                                <li>
+                                    <h4>Event: </h4>
+                                    {apiData.event || 'error'}
+                                </li>
+                                <hr />
+                                <li>
+                                    <h4>Selected: </h4>
+                                    {apiData.selected || 'error'}
+                                </li>
+                                <hr />
+                                <li>
+                                    <h4>Who was born: </h4>
+                                    {apiData.birth || 'error'}
+                                </li>
+                                <hr />
+                                <li>
+                                    <h4>Who died: </h4>
+                                    {apiData.death || 'error'}
+                                </li>
+                                <hr />
+                                <li>
+                                    <h4>Holiday:</h4>
+                                    {apiData.holiday || 'error'}
+                                </li>
+                            </ul>
+                        </div>)}
                 </div>
             </main>
         </>
